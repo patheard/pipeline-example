064722661ae1d8ea8e59048678c26880bec1b474
Pat Heard <patrick.heard@tpsgc-pwgsc.gc.ca>
2019-01-02 14:19:06 -0500

    Working front-end using npm server

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-// Import the library
+/** A simple HTTP server */
+
 const server = require('server');
 const { get, post, put, del } = server.router;
 const { render } = server.reply;
 
 // Render the homepage for `/`
-const home = get('/', ctx => render('src/views/index.html'));
+const home = get('/', ctx => render('index.html'));
+
+// Launch the server
+server(home);
