6d9d7b3f46f96adc37f63a351710bdcbd543e962
Dave Schindler <dave.schindler@gmail.com>
2019-01-04 14:17:56 -0500

    switched to local gitlab runner; moved deploy to heroku

---
+++ gitlab-runner.bat
@@ -0,0 +1,1 @@
+docker run -d --name gitlab-runner --restart always gitlab/gitlab-runner:latest
---
+++ gitlab-unregister-runner.bat
@@ -0,0 +1,1 @@
+docker exec -it gitlab-runner bash -c "gitlab-runner unregister --url https://gitlab.com --name local-runner"
